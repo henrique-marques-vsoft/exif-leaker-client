0aa015fe044c5646d223adbd6497a00024d55704
hubconfeccao <allyson2308h@gmail.com>
2023-01-10 02:42:36 -0300

    feat: add about page

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,15 +1,40 @@
 <template>
   <v-app-bar :elevation="2">
-    <v-app-bar-title>EXIF Leaker</v-app-bar-title>
+    <v-app-bar-title>
+      <router-link to="/">
+        <div class="link-container">
+          <p>EXIF Leaker</p>
+        </div>
+      </router-link>
+    </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script lang="ts">
   export default {
     data () {
-      return {
-        
-      }
+      return {}
     },
   }
 </script>
+
+<style scoped>
+a {
+  color: white;
+  text-decoration: none;
+  font-size: 22px;
+  font-weight: bold;
+}
+.link-container {
+  display: flex;
+  justify-content: space-between;
+  padding-right: 1%;
+  align-items: center;
+  transition: 500ms;
+  width: 12%;
+  height: 100vh;
+}
+.link-container:hover {
+  color: goldenrod;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,74 @@
 <template>
-  <h1>but this is about</h1>
-  <HelloWorld />
+  <TopBar />
+  <div class="body">
+    <div class="img-container">
+      <img src="1.webp" alt="Girl in a jacket">
+    </div>
+    <p class="prompt-desc">AI Generated Image by Stable Diffusion AI. Prompt: Slow shutter speed photo of a cyberpunk scenery, architecture, public realm, jonathan solte and zac retz style</p>
+    <div class="description-container">
+      <h1>About this project</h1>
+      <h3>You can contribute to this project! Feel free to contribute or open an issue on Github.</h3>
+      <a href="https://github.com/all123all/exif-leaker-client" target="_blank" rel="noopener noreferrer">Frontend here.</a>
+      <a href="https://github.com/all123all/exif-leaker" target="_blank" rel="noopener noreferrer">Backend here.</a>
+      <a href="https://github.com/all123all" target="_blank" rel="noopener noreferrer">Other projects here.</a>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import TopBar from '@/components/TopBar.vue'
 </script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+h3 {
+  margin-bottom: 2%;
+}
+a {
+  color: goldenrod;
+  text-decoration: none;
+  transition: 500ms;
+}
+a:hover {
+  color: white;
+  font-size: 20px;
+}
+.body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 0 15%;
+}
+.img-container {
+  margin-top: 2%;
+  border-radius: 10px;
+  border-width: 2px;
+  border-style: solid;
+  overflow: auto;
+  width: 100%;
+  height: 200px;
+  background-size: contain;
+}
+.prompt-desc {
+  margin-top: 0.5%;
+  font-size: 12px;
+  color: gray;
+  opacity: 50%;
+}
+.description-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 1.5% 0;
+}
+.link-container {
+  background-color: aquamarine;
+}
+</style>
